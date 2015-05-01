9c9015b0f927007f378f9f6cdabe6c7d69836d6f
Isaías <isaiahlima18@gmail.com>
2015-05-01 11:03:19 -0300

    Todas as conexões necessárias programadas

--- iGit/PullDetail.swift
+++ iGit/PullDetail.swift
@@ -1,5 +1,5 @@
 //
-//  PullSearch.swift
+//  PullDetail.swift
 //  iGit
 //
 //  Created by Isaías Lima on 01/05/15.
@@ -8,43 +8,39 @@
 
 import UIKit
 
-class PullSearch:  NSObject, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
+class PullDetail: NSObject, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
     
     var json: NSMutableData!
-    var pullRequests: NSMutableArray!
-    var pullNumber: Int!
-    var mackSearch: MackSearch!
-    var usuario: User!
-    var repositorio: String!
-    var i = 1
+    var pull: NSDictionary!
+    var labels: NSMutableArray!
+    var pullSearch: PullSearch!
     
-    class var sharedInstance : PullSearch {
+    
+    
+    class var sharedInstance : PullDetail {
         struct Static {
             static var onceToken : dispatch_once_t = 0
-            static var instance : PullSearch? = nil
+            static var instance : PullDetail? = nil
         }
         dispatch_once(&Static.onceToken) {
-            Static.instance = PullSearch()
+            Static.instance = PullDetail()
         }
         return Static.instance!
     }
     
     private override init() {
-        mackSearch = MackSearch.sharedInstance
+        pullSearch = PullSearch.sharedInstance
         super.init()
     }
     
-    func pullGitHub(user: User, repo: String) {
-        
-        usuario = user
-        repositorio = repo
+    func labelPull(user: User, repo: String, pullNumber: Int) {
         
         let loginString = NSString(format: "%@:%@", user.nome, user.senha)
         let loginData: NSData = loginString.dataUsingEncoding(NSUTF8StringEncoding)!
         let base64LoginString = loginData.base64EncodedStringWithOptions(nil)
         
         
-        let url = NSURL(string: "https://api.github.com/repos/mackmobile/\(repo)/pulls?state=all&page=\(i)")
+        let url = NSURL(string: "https://api.github.com/repos/mackmobile/\(repo)/issues/\(pullNumber)")
         let request = NSMutableURLRequest(URL: url!)
         request.HTTPMethod = "GET"
         request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
@@ -53,7 +49,7 @@ class PullSearch:  NSObject, NSURLConnectionDelegate, NSURLConnectionDataDelegat
         // make sure your class conforms to NSURLConnectionDelegate
         let urlConnection = NSURLConnection(request: request, delegate: self, startImmediately: true)
         urlConnection?.start()
-        i++
+        
     }
     
     func connection(connection: NSURLConnection, didReceiveData data: NSData) {
@@ -65,25 +61,8 @@ class PullSearch:  NSObject, NSURLConnectionDelegate, NSURLConnectionDataDelegat
     }
     
     func connectionDidFinishLoading(connection: NSURLConnection) {
-        pullRequests = NSJSONSerialization.JSONObjectWithData(json, options: .MutableContainers, error: nil) as! NSMutableArray
-        
-        if pullRequests != nil {
-            
-            for j in 0..<(pullRequests.count) {
-                
-                var pullRequest = pullRequests[j].objectForKey("user") as! NSMutableDictionary!
-                if pullRequest.objectForKey("login") as! String == usuario.nome {
-                    pullNumber = pullRequests[j].objectForKey("number") as! Int
-                }
-                
-            }
-            
-        } else {
-            self.pullGitHub(usuario, repo: repositorio)
-        }
-        
+        pull = NSJSONSerialization.JSONObjectWithData(json, options: .MutableContainers, error: nil) as! NSDictionary
+        labels = pull.objectForKey("labels") as! NSMutableArray
     }
-    
-    
     
 }
